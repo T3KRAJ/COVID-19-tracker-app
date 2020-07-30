d6bf560bc5d487d8fce49a297d90cf62e7e9c67e
Tekraj Joshi <tekrajjoshi07@gmail.com>
2020-07-30 14:04:18 +0530

    Global data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {Cards, Chart, CountryPicker} from './components'
+import styles from './App.module.css'
+import { fetchData } from './api'
+class App extends Component{
+  state = {
+    data: {},
+  }
 
-function App() {
-  return (
-    <div className="App">
+  async componentDidMount(){
+    const fetchedData = await fetchData()
 
-    </div>
-  );
+    this.setState({ data: fetchedData })
+  }
+  render(){
+    const {data} = this.state
+    return(
+      <div className={styles.container}>
+        <Cards data={data}/>
+        {/* <Chart /> */}
+        <CountryPicker />
+      </div>
+    )
+  }
 }
 
 export default App;
